51a5356772c5a4fe59f97ecd56a5ad4d2589974a
Haleynp <haleynpatel@gmail.com>
2023-07-19 10:11:15 -0400

    Add project submission folder back

---
+++ TermProjectSubmission/customeraccounts.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>HS Banks</title>
+    <nav>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="customerdata.php">View Customers</a></li>
+        <li><a href="customeraccounts.php">View Accounts</a></li>
+        <li><a href="createcustomer.php">Create Customer</a></li>
+    </nav>
+</head>
+<!-- include css and js files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+crossorigin="anonymous"></script>
+<link rel="stylesheet" href="styles.css">
+
+
+<body>
+    <div>
+        <h3>View Accounts</h3>
+        <form action="viewaccounts.php" method="post">
+            <label for="cssn">Enter Customer SSN</label><br>
+            <input type="text" id="c" name="c"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</body>
+
+
---
+++ TermProjectSubmission/viewaccounts.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>HS Banks</title>
+    <nav>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="customerdata.php">View Customers</a></li>
+        <li><a href="customeraccounts.php">View Accounts</a></li>
+        <li><a href="createcustomer.php">Create Customer</a></li>
+    </nav>
+</head>
+<!-- include css and js files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+crossorigin="anonymous"></script>
+<link rel="stylesheet" href="styles.css">
+
+<body>
+    <div>
+        <form action="viewaccounts.php" method="post">
+            <label for="cssn">Enter Customer SSN</label><br>
+            <input type="text" id="c" name="c"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</body>
+<?php
+        $cnx = new mysqli('localhost', 'cs331', 'termproject' , 'cs331');
+
+
+
+
+        if ($cnx->connect_error)
+            die('Connection failed: ' . $cnx->connect_error);
+
+
+
+
+        $cssn = $_POST['c'];
+        $query = "SELECT * FROM HAS WHERE cssn=$cssn";
+        $cursor = $cnx->query($query);
+    ?>
+    <div>
+        <table>
+            <tr>
+                <th>Account Number</th>
+            </tr>
+            <?php while($row = $cursor->fetch_assoc()) : ?>
+                <tr>
+                    <td><?php echo $row['a_number']; ?></td>
+                    <td align="center">
+                        <form action="transactions.php" method="POST">
+                            <button type="submit" id="view" name="view" value="<?php echo $row['a_number']; ?>">View Transactions</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
+    </div>
+    <?php
+        $cnx->close();
+    ?>
